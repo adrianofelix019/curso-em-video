4067f03c34e0a4575f1f965dbf1aa6f856611490
Adriano Felix <adrianofelix019@gmail.com>
2024-02-22 19:55:50 -0300

    Melhoria na lógica de comparação de triângulo

--- desafios/desafio042.py
+++ desafios/desafio042.py
@@ -13,14 +13,14 @@ lado3 = int(input("Terceiro lado: "))
 
 if (lado1 + lado2 > lado3) and (lado2 + lado3 > lado1) and (lado1 + lado3 > lado2):
     print(f"É possível formar um triângulo com ({lado1}, {lado2}, {lado3})")
+
+    if lado1 == lado2 == lado3:
+        tipo_de_triangulo = "equilátero"
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        tipo_de_triangulo = "isósceles"
+    else:
+        tipo_de_triangulo = "escaleno"
+
+    print(f"O triângulo formado será {tipo_de_triangulo}.")
 else:
     print(f"Não é possível formar um triângulo com ({lado1}, {lado2}, {lado3})")
-
-if lado1 == lado2 and lado2 == lado3:
-    tipo_de_triangulo = "equilátero"
-elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
-    tipo_de_triangulo = "isósceles"
-else:
-    tipo_de_triangulo = "escaleno"
-
-print(f"O triângulo formado será {tipo_de_triangulo}.")
