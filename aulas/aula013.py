30e47c04a88b814bcc34c55a5a0d77523f0279ca
Adriano Felix <adrianofelix019@gmail.com>
2024-03-05 14:48:53 -0300

    refatora aula013.py

--- aulas/aula013.py
+++ aulas/aula013.py
@@ -1,6 +1,16 @@
 soma = 0
 
 for c in range(0, 3):
-    n = int(input('Informe um valor: '))
-    soma += n
+    numero = int(input('Informe um valor: '))
+    soma += numero
 print(f'O somatório de todos os valores foi {soma}')
+
+
+def average(values: list[float]) -> float:
+    sum = 0
+    for value in values:
+        sum += value
+    return sum / len(values)
+
+
+print(average([9.5, 10.0]))
