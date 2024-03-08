e5297b93ee6e185e1ec31ec01b9304583bea4eef
Adriano Felix <adrianofelix019@gmail.com>
2024-03-08 00:23:34 -0300

    concluído desafio 005

---
+++ desafios/desafio005.py
@@ -0,0 +1,9 @@
+"""
+Esse script lê um número e mostra seu sucessor e antecessor.
+"""
+
+num = int(input('Informe um número: '))
+sucessor = num + 1
+antecessor = num - 1
+print('O antecessor de {} é {}.'.format(num, antecessor))
+print('O sucessor de {} é {}.'.format(num, sucessor))
