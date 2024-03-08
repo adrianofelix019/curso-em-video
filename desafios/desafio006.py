96d6b89d632e74e50bf7faaa75311ed283bab84a
Adriano Felix <adrianofelix019@gmail.com>
2024-03-08 00:23:47 -0300

    concluído desafio 006

---
+++ desafios/desafio006.py
@@ -0,0 +1,11 @@
+"""
+Esse script lê um número e mostra seu dobro, triplo e raiz quadrada.
+"""
+
+num = int(input('Informe um número: '))
+dobro = num * 2
+triplo = num * 3
+raiz_quadrada = num ** 0.5
+print('O dobro de {} é {}'.format(num, dobro))
+print('O triplo de {} é {}'.format(num, triplo))
+print('A raiz quadrada de {} é {}'.format(num, raiz_quadrada))
