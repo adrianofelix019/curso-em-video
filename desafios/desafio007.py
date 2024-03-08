612ed525bb27c7fc80765e609f5ecdd9263895fb
Adriano Felix <adrianofelix019@gmail.com>
2024-03-08 00:23:57 -0300

    concluído desafio 007

---
+++ desafios/desafio007.py
@@ -0,0 +1,8 @@
+"""
+Esse script lê duas notas e mostra a média.
+"""
+
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+media = (n1 + n2) / 2
+print('A média é {:.1f}'.format(media))
