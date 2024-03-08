b08bae760a82c6b686229a0dacd5f090b062d98a
Adriano Felix <adrianofelix019@gmail.com>
2024-03-08 00:24:12 -0300

    concluído desafio 008

---
+++ desafios/desafio008.py
@@ -0,0 +1,11 @@
+"""
+Esse script ler um valor em metros e mostra na tela a conversão para centimetros
+e milimetros.
+"""
+
+
+metros = float(input('Informe um valor em metros: '))
+centimetros = metros * 100
+milimetros = metros * 1000
+print('{}m convertido para centimetros vale {}cm.'.format(metros, centimetros))
+print('{}m convertido para milimetros vale {}mm'.format(metros, milimetros))
