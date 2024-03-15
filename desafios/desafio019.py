203750e4ee60520bf27547238104f78a19b66da0
Adriano Felix <adrianofelix019@gmail.com>
2024-03-15 00:07:25 -0300

    modificação no desafio 019

--- desafios/desafio019.py
+++ desafios/desafio019.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 
 """
 Script para sortear um nome entre quatro alunos.
@@ -9,6 +9,7 @@ aluno1 = input('Informe o nome do 1° aluno(a): ')
 aluno2 = input('Informe o nome do 2° aluno(a): ')
 aluno3 = input('Informe o nome do 3° aluno(a): ')
 aluno4 = input('Informe o nome do 4° aluno(a): ')
-aluno_sorteado = random.randint(1, 4)
+# aluno_sorteado = random.randint(1, 4)
 alunos = [aluno1, aluno2, aluno3, aluno4]
-print('O aluno(a) sorteado foi {}.'.format(alunos[aluno_sorteado]))
+aluno_sorteado = choice(alunos)
+print('O aluno(a) sorteado foi {}.'.format(aluno_sorteado))
