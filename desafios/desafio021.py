f599bf54f761517751be95f4f3e68a9e4ff63b41
Adriano Felix <adrianofelix019@gmail.com>
2024-03-16 00:49:33 -0300

    alteração no desafio 021

--- desafios/desafio021.py
+++ desafios/desafio021.py
@@ -5,8 +5,10 @@ from time import sleep
 Esse script abre e reproduz um arquivo mp3.
 """
 
-pygame.mixer.init()
-pygame.mixer.music.load('song.mp3')
-pygame.mixer.music.play(-1)
+pygame.init()
+musica = pygame.mixer.Sound('song.mp3')
+pygame.mixer.Sound.play(musica)
+duracao_da_musica = int(pygame.mixer.Sound.get_length(musica)) * 1000
+print('Tocando música selecionada no código.')
 print('Pressione CTRL+C para sair.')
-sleep(1000)
+sleep(duracao_da_musica)
