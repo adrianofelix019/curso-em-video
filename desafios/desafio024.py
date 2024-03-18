dda70d4a71bd2ce715d34eb16db93fadf96d7be1
Adriano Felix <adrianofelix019@gmail.com>
2024-03-18 14:01:29 -0300

    alteração no desafio 025

--- desafios/desafio024.py
+++ desafios/desafio024.py
@@ -3,6 +3,6 @@ Esse programa lê o nome de uma cidade pelo teclado e verifica se ele começa
 com Santo.
 """
 
-nome = input('Informe o nome da cidade: ')
-print('A cidade começa com Santo: ', end='')
-print(nome.startswith('Santo'))
+nome = input('Informe o nome da cidade: ').strip().lower()
+print('A cidade começa com Santo? ', end='')
+print(nome.startswith('santo'))
