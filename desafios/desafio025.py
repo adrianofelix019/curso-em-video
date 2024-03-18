7ce58a7ac5f9f0cf30049e516299e811ecea7bbf
Adriano Felix <adrianofelix019@gmail.com>
2024-03-18 00:40:57 -0300

    concluído desafio 025

---
+++ desafios/desafio025.py
@@ -0,0 +1,9 @@
+"""
+Programa lê o nome de uma pessoa pelo teclado e mostra na tela se tem Silva no
+nome.
+"""
+
+
+nome = input('Escreva seu nome: ')
+print('Você tem Silva no nome? ', end='')
+print('silva' in nome.lower())
