abf12fc356cd0e7712182b60d738af062e370364
Adriano Felix <adrianofelix019@gmail.com>
2024-03-18 13:15:53 -0300

    alteração no desafio 022

--- desafios/desafio022.py
+++ desafios/desafio022.py
@@ -8,7 +8,8 @@ Esse programa lê pelo teclado o nome completo de uma pessoa e mostra na tela:
 """
 
 nome = input('Informe seu nome completo: ')
-print(nome.upper())
-print(nome.lower())
+print('Analisando seu nome...')
+print('Seu nome em maiúsculas é {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
 print('O seu nome tem {} letras ao todo.'.format(len(nome.replace(' ', ''))))
 print('O seu primeiro nome tem {} letras.'.format(len(nome.split()[0])))
