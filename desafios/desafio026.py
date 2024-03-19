51b7ec80de1cdc477e31ad2fe1ccb5a8416237e5
Adriano Felix <adrianofelix019@gmail.com>
2024-03-19 00:31:34 -0300

    alteração no desafio 026

--- desafios/desafio026.py
+++ desafios/desafio026.py
@@ -7,7 +7,9 @@ Esse programa lê uma frase pelo teclado e mostra na tela:
 """
 
 
-frase = input('Digite qualquer coisa: ')
+frase = input('Digite qualquer coisa: ').strip()
 print('A letra A aparece {} vezes.'.format(frase.lower().count('a')))
-print('Ela aparece pela primeira vez no índice {}'.format(frase.lower().find('a')))
-print('Ela aparece pela últma vez no índice {}'.format(frase.lower().rfind('a')))
+primeira_ocorrencia = frase.lower().find('a') + 1
+ultima_ocorrencia = frase.lower().rfind('a') + 1
+print('Ela aparece pela primeira vez no índice {}'.format(primeira_ocorrencia))
+print('Ela aparece pela últma vez no índice {}'.format(ultima_ocorrencia))
