79a2530ad4a550b2563b833b4d5820272d409abb
Adriano Felix <adrianofelix019@gmail.com>
2024-03-23 12:35:01 -0300

    alteração no desafio 032

--- desafios/desafio032.py
+++ desafios/desafio032.py
@@ -1,9 +1,15 @@
+import datetime
+
 """
 Programa para verificar se um ano é bissexto.
 """
 
 
+print('Digite 0 para verificar o ano atual.')
 ano = int(input('Informe o ano para verificar se é bissexto: '))
+
+if ano == 0:
+    ano = datetime.datetime.today().year
 
 if ano % 4 == 0 and ano % 100 != 0:
     print(f'{ano} é bissexto.')
