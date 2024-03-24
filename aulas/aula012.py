6ddbc25ee4fedb2a4441e8ca7efeaf37efefbe06
Adriano Felix <adrianofelix019@gmail.com>
2024-03-24 12:00:09 -0300

    alteração na aula012

--- aulas/aula012.py
+++ aulas/aula012.py
@@ -1,10 +1,12 @@
-nome = str(input("Qual seu nome? "))
+nome = str(input("Qual é o seu nome? "))
+
 if nome == "Vitor":
     print("Que nome bonito!")
-elif nome in ["Pedro", "Maria", "Paulo"]:
+elif nome in "Pedro Maria Paulo":
     print("Seu nome é bem popular no Brasil.")
-elif nome in "Ana Cláudia Jéssica Juliana":
+elif nome in "Ana Claúdia Jéssica e Juliana":
     print("Belo nome feminino")
 else:
-    print("Seu nome é bem chato!")
-print(f"Tenha um bom dia, {nome}!")
+    print("Seu nome é bem normal!")
+
+print("Tenha um bom dia, {}!".format(nome))
