07574c0e0874f72ee215e17d03bb97d4611ff9d6
Adriano Felix <adrianofelix019@gmail.com>
2024-03-28 04:12:09 -0300

    modificação no desafio 040

--- desafios/desafio040.py
+++ desafios/desafio040.py
@@ -1,5 +1,5 @@
 """
-Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando
+Esse programa lê duas notas de um aluno e calcula sua média, mostrando
 uma mensagem no final, de acordo com a média atingida.
 
 - Média abaixo de 5.0: REPROVADO
