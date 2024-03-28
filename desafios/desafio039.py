1bbfc800241728f35f2349ffa71e359a4f1f2a54
Adriano Felix <adrianofelix019@gmail.com>
2024-03-28 04:11:57 -0300

    modificação no desafio 039

--- desafios/desafio039.py
+++ desafios/desafio039.py
@@ -1,13 +1,14 @@
 from datetime import date
+
 """
-Faça um programa que leia o ano de nascimento de um jovem e informe de acordo
+Programa que lê o ano de nascimento de um jovem e informa de acordo
 com sua idade:
 
 - Se ele ainda vai se alistar ao serviço militar
 - Se é a hora de se alistar
 - Se já passou do tempo de alistamento
 
-Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+Também será exibido o tempo que falta ou que passou do prazo.
 """
 
 ano_de_nascimento = int(input("Ano de nascimento "))
