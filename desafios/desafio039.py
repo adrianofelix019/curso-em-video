f09df8d3458306fe54687a555fe5864bfd63f78d
Adriano Felix <adrianofelix019@gmail.com>
2024-04-09 06:00:01 -0300

    modificação no desafio 039

--- desafios/desafio039.py
+++ desafios/desafio039.py
@@ -11,18 +11,23 @@ com sua idade:
 Também será exibido o tempo que falta ou que passou do prazo.
 """
 
-ano_de_nascimento = int(input("Ano de nascimento "))
-ano_atual = date.today().year
-idade = ano_atual - ano_de_nascimento
-tempo_alistamento = idade - 18
-print(f"Quem nasceu em {ano_de_nascimento} tem {idade} anos.")
-if idade < 18:
-    print("Ainda não chegou a hora de se alistar.")
-    print(f"Faltam {abs(tempo_alistamento)} anos.")
-    print(f"Seu alistamento será no ano de {ano_de_nascimento + 18}.")
-elif idade == 18:
-    print("Chegou a hora de se alistar.")
+genero = input("Informs eu seu genero (M ou F): ")
+
+if genero.lower() == "f":
+    print("Mulheres não tem alistamento obrigatório.")
 else:
-    print("Já passou o tempo de se alistar.")
-    print(f"Já passaram {tempo_alistamento} anos.")
-    print(f"Seu alistamento deveria ter sido no ano de {ano_de_nascimento + 18}.")
+    ano_de_nascimento = int(input("Ano de nascimento "))
+    ano_atual = date.today().year
+    idade = ano_atual - ano_de_nascimento
+    tempo_alistamento = idade - 18
+    print(f"Quem nasceu em {ano_de_nascimento} tem {idade} anos.")
+    if idade < 18:
+        print("Ainda não chegou a hora de se alistar.")
+        print(f"Faltam {abs(tempo_alistamento)} anos.")
+        print(f"Seu alistamento será no ano de {ano_de_nascimento + 18}.")
+    elif idade == 18:
+        print("Chegou a hora de se alistar.")
+    else:
+        print("Já passou o tempo de se alistar.")
+        print(f"Já passaram {tempo_alistamento} anos.")
+        print(f"Seu alistamento deveria ter sido no ano de {ano_de_nascimento + 18}.")
