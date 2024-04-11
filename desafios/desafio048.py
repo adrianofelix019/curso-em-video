467a86f655e00ff614794413a39496a376805766
Adriano Felix <adrianofelix019@gmail.com>
2024-04-11 19:53:34 -0300

    concluído desafio 048

---
+++ desafios/desafio048.py
@@ -0,0 +1,11 @@
+"""
+Esse script faz uma contagem regressiva de 10 até 0 e mostra uma mensagem de
+feliz ano novo.
+"""
+
+from time import sleep
+
+for c in range(10, 0, -1):
+    print("{}!".format(c))
+    sleep(1)
+print("Feliz ano novo!!!")
