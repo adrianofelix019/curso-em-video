149533a62122e36999bb9bb72bbc72adbe2dc6af
Adriano Felix <adrianofelix019@gmail.com>
2024-04-23 12:31:30 -0300

    alteração no desafio 048

--- desafios/desafio048.py
+++ desafios/desafio048.py
@@ -3,9 +3,12 @@ Esse script mostra na tela a soma de todos os números ímpares que são multipl
 de 3 e se encontram no intervalo entre 1 e 500
 """
 soma = 0
+contador = 0
 for c in range(1, 501, 2):
     if c % 3 == 0:
+        contador += 1
         soma += c
 
-print("A soma de todos os número ímpares e multiplos de 3 entre 1 e 500")
+print("A soma de todos os {} números ímpares e multiplos de 3 entre 1 e 500"
+      .format(contador))
 print("É igual a {}".format(soma))
