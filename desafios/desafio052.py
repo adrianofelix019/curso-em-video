856b445b3de434be6ced0f37ae6bfdcc8fe0d169
Adriano Felix <adrianofelix019@gmail.com>
2024-04-23 14:11:39 -0300

    alteração no desafio 052

--- desafios/desafio052.py
+++ desafios/desafio052.py
@@ -6,7 +6,20 @@ primo.
 n = int(input('Informe um número para verificar se ele é primo: '))
 
 e_primo = True
-for c in range(2, n):
+divisores = 0
+
+for c in range(1, n + 1):
+
     if n % c == 0:
-        e_primo = False
-print('O número {} é primo? {}'.format(n, e_primo))
+        divisores += 1
+        print('\033[92m{}\033[0m'.format(c), end=' ')
+    else:
+        print('\033[91m{}\033[0m'.format(c), end=' ')
+
+print('\nO número {} é divisível por {} números'.format(n, divisores))
+print('E por isso ele ', end='')
+
+if divisores == 2:
+    print('É PRIMO')
+else:
+    print('NÃO É PRIMO')
