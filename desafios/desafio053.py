072308c255bbed73e77ed0ade81522973c026fd8
Adriano Felix <adrianofelix019@gmail.com>
2024-04-23 14:27:37 -0300

    alteração no desafio 053

--- desafios/desafio053.py
+++ desafios/desafio053.py
@@ -3,9 +3,10 @@ Esse programa lê uma frase qualquer pelo teclado e informa se ela é um
 palindromo.
 '''
 
-frase = str(input('Digite algo: '))
-frase_reversa = frase[::-1].replace(' ', '')
+frase = str(input('Digite algo: ')).upper()
+frase_reversa = frase[::-1].replace(' ', '').upper()
 
+print('O inverso de {} é {}'.format(frase, frase_reversa))
 if frase_reversa == frase.replace(' ', ''):
     print('{} é um palindromo.'.format(frase))
 else:
