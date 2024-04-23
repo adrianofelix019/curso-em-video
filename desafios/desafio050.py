e3a9913c56bf4f988b9046ad5d038291fa5018de
Adriano Felix <adrianofelix019@gmail.com>
2024-04-23 12:44:00 -0300

    alteração no desafio 050

--- desafios/desafio050.py
+++ desafios/desafio050.py
@@ -4,9 +4,13 @@ Se o valor digitado for ímpar ela será desconsiderado.
 '''
 
 soma = 0
+contador = 0
 for c in range(0, 6):
     n = int(input('Digite o {}º valor: '.format(c+1)))
 
     if n % 2 == 0:
         soma += n
-print('A soma de todos os valores pares é igual a {}'.format(soma))
+        contador += 1
+
+print('Você informou {} números \033[1mpares\033[0m e a soma é igual a {}'
+      .format(contador, soma))
