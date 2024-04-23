0dc4ab7d8fd48bda8c3b6a823399e6a08a041228
Adriano Felix <adrianofelix019@gmail.com>
2024-04-23 13:03:33 -0300

    alteração no desafio 051

--- desafios/desafio051.py
+++ desafios/desafio051.py
@@ -5,7 +5,8 @@ mostra na tela os 10 primeiros termos dessa progressão.
 
 primeiro_termo = int(input('Informe o primeiro termo da PA: '))
 razao = int(input('Informe a razão da PA: '))
-proximo_termo = primeiro_termo
-for c in range(0, 10):
-    print(proximo_termo, end=', ')
-    proximo_termo += razao
+decimo_termo = primeiro_termo + (10 - 1) * razao
+for c in range(primeiro_termo, decimo_termo + 1, razao):
+    print(c, end=' ➝  ')
+
+print('FIM')
