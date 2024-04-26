213788651cca8fc5a28b4b6fe162614850d9d63f
Adriano Felix <adrianofelix019@gmail.com>
2024-04-26 15:03:50 -0300

    alteração no desafio 057

--- desafios/desafio057.py
+++ desafios/desafio057.py
@@ -3,8 +3,9 @@ Esse programa lê um sexo de uma pessoa mas só aceita os valores 'M' ou 'F'
 caso um valor incorreto seja inserido terá que digitar novamente.
 '''
 
-sexo = ''
-while sexo != 'M' and sexo != 'F':
-    sexo = input('Informe seu sexo: [F/M] ').upper()
-
-print(f'Seu sexo é {sexo}')
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(
+        input('Dados inválidos, por favor informe seu sexo: ')
+    ).strip().upper()[0]
+print('Sexo {} registrado com sucesso.'.format(sexo))
