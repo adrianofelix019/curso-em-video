5f375683ce49c1d59156e020237597a568db13da
Adriano Felix <adrianofelix019@gmail.com>
2024-04-30 14:39:22 -0300

    alteração no desafio 062

--- desafios/desafio062.py
+++ desafios/desafio062.py
@@ -1,21 +1,18 @@
+print('-=' * 16)
+print('Gerador de progressão aritmetica')
+print('-=' * 16)
 primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
-decimo = primeiro + 10 * razao
-
-contador = primeiro
-while contador <= decimo:
-    print(contador, end=' -> ')
-    contador += razao
-
-    if contador == decimo:
-        continuar = int(
-            input('Quer visualizar mais termos? (Digite 0 para sair) ')
-        )
-        print()
-        if continuar != 0:
-            contador = decimo
-            decimo = decimo + (continuar) * razao
-        else:
-            print('Saindo...')
-
-print('FIM')
+termo = primeiro
+cont = 1
+mais = 10
+total = 0
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} ➝  '.format(termo), end='')
+        cont += 1
+        termo += razao
+    print('PAUSA')
+    mais = int(input('Quantos termos a mais deseja visualizar? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
