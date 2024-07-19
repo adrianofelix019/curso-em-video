301b265e31be128e55efd8196672c1738b8288ba
Adriano Felix <adrianofelix019@gmail.com>
2024-07-19 10:16:18 -0300

    alteração no desafio 69

--- desafios/desafio69.py
+++ desafios/desafio69.py
@@ -7,8 +7,10 @@ while True:
     print(f'{"CADASTRE UMA PESSOA":^60}')
     print('-'*60)
     idade = int(input('Informe sua idade: '))
-    sexo = str(input('Informe seu sexo (M/F): '))
-    continuar = str(input('Quer continuar? (S/N) ')).strip()
+
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Informe seu sexo (M/F): ')).strip().upper()[0]
 
     if idade > 18:
         maiores_de_idade += 1
@@ -19,11 +21,14 @@ while True:
     if sexo in 'Ff' and idade < 20:
         mulheres_com_menos_de_20_anos += 1
 
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? (S/N) ')).strip().upper()[0]
+
     if continuar in 'Nn':
         print('Saindo...')
         break
 
 print(f'Ao todo foram cadastrados {maiores_de_idade} maiores de idade.')
-print(f'E {homens} homens foram cadastrados.')
-print(f'E também {mulheres_com_menos_de_20_anos}', end='')
-print('Mulheres com menos de 20 anos foram cadastradas.')
+print(f'Total de homens cadastrados: {homens}')
+print(f'Total de mulheres com menos de 20 anos cadastradas: {mulheres_com_menos_de_20_anos}')
