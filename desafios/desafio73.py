853650749e73bea9a0daa7d502f9165d79c2565e
Adriano Felix <adrianofelix019@gmail.com>
2024-07-28 17:45:52 -0300

    alteração no desafio 73

--- desafios/desafio73.py
+++ desafios/desafio73.py
@@ -22,19 +22,29 @@ brasileirão = (
 )
 
 print(f'Ao todo temos {len(brasileirão)} times competindo no Brasileirão.')
-print('Os 5 primeiros colocados do Brasileirão são:')
-for time in brasileirão[:5]:
-    print(time)
-
-print('Os 4 últimos colocados são:')
-for i in range(len(brasileirão) - 4, len(brasileirão)):
-    brasileirão[i]
+for indice, time in enumerate(brasileirão):
+    colocação = str(indice + 1) + '°'
+    print(f'{colocação:<3} - {time}')
 
 print('-='*25)
+
+print('Os 5 primeiros colocados do Brasileirão são:')
+for indice, time in enumerate(brasileirão[:5]):
+    colocação = str(indice + 1) + '°'
+    print(f'{colocação:<2} - {time}')
+
+print('-='*25)
+
+print('Os 4 últimos colocados são:')
+for indice, time in enumerate(brasileirão[-4:]):
+    colocação = str(len(brasileirão) - (4 - (indice + 1))) + '°'
+    print(f'{colocação:<2} - {time}')
+
+print('-='*25)
+
 print('Os times do Brasileirão em ordem alfabética:')
 for time in sorted(brasileirão):
     print(time)
 
-# TODO: Em que posição da tabela está o time da Chapecoense.
 print('-='*25)
-print(f'O Vasco da Gama está na {brasileirão.index('Vasco da Gama')}ª posição.')
+print(f'O Vasco da Gama está na {brasileirão.index('Vasco da Gama')+1}ª posição.')
