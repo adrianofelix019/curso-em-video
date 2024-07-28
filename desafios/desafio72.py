7c3591ea10d4f44f42cd05123ed30ed2b3234282
Adriano Felix <adrianofelix019@gmail.com>
2024-07-28 17:16:51 -0300

    alteração no desafio 72

--- desafios/desafio72.py
+++ desafios/desafio72.py
@@ -1,5 +1,19 @@
-numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-numero = -1
-while numero not in range(0, 20):
-    numero = int(input('Digite um número 0 e 20: '))
-print(f'Você digitou o número {numeros[numero]}.')
+numeros_por_extenso = (
+    'Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 
+    'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 
+    'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 
+    'Dezessete', 'Dezoito', 'Dezenove', 'Vinte'
+)
+
+continuar = 's'
+
+while continuar in 'Ss':
+    numero = -1
+    while numero not in range(0, 21):
+        numero = int(input('Digite um número 0 e 20: '))
+    print(f'Você digitou o número {numeros_por_extenso[numero]}.')
+
+    continuar = ' '
+    while continuar not in 'SsNn':
+        continuar = input('Quer continuar? (S/N): ').strip()[0]
+print('Saindo...')
