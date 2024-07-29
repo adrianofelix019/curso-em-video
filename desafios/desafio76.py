7069a2460b38a35059ac8deb74f175121bdf233f
Adriano Felix <adrianofelix019@gmail.com>
2024-07-29 12:55:40 -0300

    alteração no desafio 76

--- desafios/desafio76.py
+++ desafios/desafio76.py
@@ -2,7 +2,9 @@ listagem = ('Notebook', 3499, 'Celular', 1899, 'XBox', 4379, 'Playstation 5', 31
 print('-'*50)
 print(f'{'LISTAGEM DE PREÇOS':^50}')
 print('-'*50)
-for i in range(0, len(listagem), 2):
-    if i == len(listagem) - 1:
-        break
-    print(f'{listagem[i]:.<40}R${listagem[i+1]: >10.2f}')
+
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
