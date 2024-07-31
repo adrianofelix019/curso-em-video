311b1a828888a1249067d8af15e8705ff43add4d
Adriano Felix <adrianofelix019@gmail.com>
2024-07-31 12:39:54 -0300

    alteração no desafio 77

--- desafios/desafio77.py
+++ desafios/desafio77.py
@@ -1,11 +1,20 @@
 palavras = ('banana', 'carro', 'computador', 'girafa', 'chuva', 
            'montanha', 'guitarra', 'sorriso', 'livro', 'oceano', 
-           'aventura', 'futebol', 'lua', 'floresta', 'arco-íris', 
-           'viagem', 'castelo', 'chocolate', 'telefone', 'lápis')
+           'aventura', 'futebol', 'lua', 'floresta', 'arco-iris', 
+           'viagem', 'castelo', 'chocolate', 'telefone', 'lapis')
 
 for palavra in palavras:
-    print(f'Na palavra {palavra} temos as vogais ', end='')
-    for letra in palavra:
+    print(f'Na palavra {palavra.upper()} temos as vogais: ', end='')
+
+    ultima_posição_vogal = 0
+    for indice, letra in enumerate(palavra):
         if letra.lower() in 'aeiou':
-            print(letra, end=' ')
+            ultima_posição_vogal = indice
+
+    for indice, letra in enumerate(palavra):
+        if letra.lower() in 'aeiou':
+            if indice == ultima_posição_vogal:
+                print(letra, end='')
+            else:
+                print(letra, end=', ')
     print()
