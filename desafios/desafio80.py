a248f3d83fa5908f250689312a9837c67e79c676
Adriano Vitor <adrianofelix019@gmail.com>
2024-12-17 12:42:44 -0300

    desafio 80 concluído

---
+++ desafios/desafio80.py
@@ -0,0 +1,18 @@
+valores = []
+indice_para_inserção = 0
+
+for c in range(5):
+    valor = int(input('Digite um valor: '))
+
+    if not valores or valor > max(valores):
+        valores.append(valor)
+        print(f'{valor} foi inserido no final da lista.')
+    else:
+        for indice, valor_da_lista in enumerate(valores):
+            if valor < valor_da_lista:
+                indice_para_inserção = indice
+                break
+        valores.insert(indice, valor)
+        print(f'O valor {valor} foi inserido no indice {indice}')
+
+print(f'Os valores digitados em ordem foram: {valores}')
