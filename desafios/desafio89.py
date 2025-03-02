8f93029fbbb9f9f69018e9bf1b9c2a3c65d0703d
Adriano Vitor <adrianofelix019@gmail.com>
2025-03-02 08:38:38 -0300

    desafio 89 concluído

---
+++ desafios/desafio89.py
@@ -0,0 +1,25 @@
+alunos = []
+dados = []
+
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+
+    dados.append(nome)
+    dados.append(nota1)
+    dados.append(nota2)
+    alunos.append(dados[:])
+    dados.clear()
+
+    continuar = str(input('Continuar? [S/N] ')).strip().lower()[0]
+
+    if continuar == 'n':
+        break
+
+print('-='*25)
+print(f'{"N°":<3}{"Nome":<15}{"Média":<15}')
+print('-'*33)
+for indice, aluno in enumerate(alunos):
+    media = (aluno[1] + aluno[2]) / 2
+    print(f'{indice+1:<3}{aluno[0]:<15}{media:<15}')
