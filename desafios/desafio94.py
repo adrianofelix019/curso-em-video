227c8c4c2c603f6f631f999b976fd51425a32f3c
Adriano Vitor <adrianofelix019@gmail.com>
2025-03-20 06:17:47 -0300

    alteração no desafio 94

--- desafios/desafio94.py
+++ desafios/desafio94.py
@@ -3,7 +3,13 @@ pessoas = []
 while True:
     nome = input('Nome: ')
     idade = int(input('Idade: '))
-    sexo = input('Sexo [M/F]: ').strip().lower()[0]
+
+    while True:
+        sexo = input('Sexo [M/F]: ').strip().lower()[0]
+        if sexo in 'mf':
+            break
+        else:
+            print('Por favor digite apenas M ou F.')
 
     pessoa = {
         'nome': nome,
@@ -13,12 +19,17 @@ while True:
 
     pessoas.append(pessoa)
 
-    continuar = input('Continuar? [S/N] ').lower().strip()[0]
+    while True:
+        continuar = input('Continuar? [S/N] ').lower().strip()[0]
+        if continuar in 'sn':
+            break
+        else:
+            print('Por favor responda apenas S ou N.')
+
     if continuar == 'n':
         break
 
 print('-=' * 25)
-print(f'Ao todo foram cadastradas {len(pessoas)} pessoas.')
 
 soma_idade = 0
 mulheres = []
@@ -27,16 +38,18 @@ for pessoa in pessoas:
 
     if pessoa['sexo'] == 'f':
         mulheres.append(pessoa)
+
 media_idade = soma_idade / len(pessoas)
 
-print(f'A média de idade é {media_idade:.2f}')
-print('As mulheres cadastradas foram: ')
+print(f'A) Ao todo temos {len(pessoas)} pessoas cadastradas.')
+print(f'B) A média de idade é {media_idade:.1f}')
+print('C) As mulheres cadastradas foram: ')
 for mulher in mulheres:
     print(f'\t{mulher['nome']}')
 
-print('As pessoas com idade acima da média são:')
+print('D) As pessoas com idade acima da média são:')
 for pessoa in pessoas:
     if pessoa['idade'] > media_idade:
-        print(f'\t{pessoa['nome']}')
+        print(f'\t{pessoa['nome']} com {pessoa['idade']} anos de idade.')
 print('-='*25)
 print('Finalizando...')
