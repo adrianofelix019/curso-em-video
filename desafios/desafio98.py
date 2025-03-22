771ecef9f7f891bfdcfc4d43cef03b066be86490
Adriano Vitor <adrianofelix019@gmail.com>
2025-03-22 07:19:07 -0300

    correção no acrescimo da contagem

--- desafios/desafio98.py
+++ desafios/desafio98.py
@@ -2,18 +2,22 @@ from time import sleep
 
 
 def contador(inicio, fim, passo):
+    acrescimo = 0
+
     if passo == 0:
         passo = 1
 
     if inicio > fim:
         passo = -abs(passo)
+        acrescimo = -1
     else:
         passo = abs(passo)
+        acrescimo = 1
 
     print(f'Contagem de {inicio} até {fim} de {abs(passo)} em {abs(passo)}.')
 
 
-    for c in range(inicio, fim+1, passo):
+    for c in range(inicio, fim + acrescimo, passo):
         print(f'{c} ', end='', flush=True)
         sleep(.5)
 
