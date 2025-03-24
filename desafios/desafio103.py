8180ec217f7b23ffb91efbc298da04b4f7eec22a
Adriano Vitor <adrianofelix019@gmail.com>
2025-03-24 10:39:28 -0300

    desafio 103 concluído

---
+++ desafios/desafio103.py
@@ -0,0 +1,15 @@
+def ficha(nome, qtd_gols):
+    if nome == '':
+        nome = '<desconhecido>'
+    
+    if qtd_gols == '':
+        qtd_gols = 0
+    else:
+        qtd_gols = int(qtd_gols)
+
+    print(f'O jogador {nome} fez {qtd_gols} gol(s) no campeonato')
+
+
+nome = str(input('Nome do jogador: '))
+gols = input('Número de gols: ')
+ficha(nome, gols)
