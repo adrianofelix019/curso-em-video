850989403bdd996b21296f3920f5f97c2612aa90
Adriano Vitor <adrianofelix019@gmail.com>
2025-03-26 11:19:30 -0300

    desafio 109 concluído

---
+++ desafios/desafio109/moeda.py
@@ -0,0 +1,34 @@
+def moeda(valor=0, moeda='R$'):
+    return f'{moeda}{valor:.2f}'.replace('.', ',')
+
+
+def aumentar(valor=0, aumento=0, formatado=False):
+    aumento /= 100
+    resultado = valor * (1 + aumento)
+    if formatado:
+        return moeda(resultado)
+    else:
+        return resultado
+
+
+def diminuir(valor=0, desconto=0, formatado=False):
+    desconto /= 100
+    resultado = valor * (1 - desconto)
+    if formatado:
+        return moeda(resultado)
+    else:
+        return resultado
+
+
+def dobro(valor=0, formatado=False):
+    if formatado:
+        return moeda(valor * 2)
+    else:
+        return valor * 2
+
+
+def metade(valor=0, formatado=False):
+    if formatado:
+        return moeda(valor / 2)
+    else:
+        return valor / 2
